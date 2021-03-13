26d65c7b14ab8164fac53ce7baf019e6aa6a1a06
yusk178 <youjiebupo@gmail.com>
2021-03-13 20:51:08 +0900

    変更点として二つあります。 ①データバインディングを実装するためのライブラリーをグラドルビルドに実装しました。 ②データバインディングを使用可能とする、ライブラリーを使用して、メインアクシビィティ内に、バインディングオブジェクトを生成しました。

--- app/src/main/java/com/example/questionbook/MainActivity.kt
+++ app/src/main/java/com/example/questionbook/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.questionbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.questionbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
     }
 }
