c408eb9a964b12dc9dac038855c17c51eb9f69b6
yusk178 <youjiebupo@gmail.com>
2021-03-19 19:04:31 +0900

    DB定義書での変更点が発生したため、変更箇所を修正又はテーブルの生成を行いました。問題集テーブルでの workbook_text , workbook_accuracy カラムの削除理由としては、問題文テーブル・正答率テーブルの各テーブルに分離のため問題種テーブルから削除しました。

--- app/src/main/java/com/example/questionbook/room/Entity.kt
+++ app/src/main/java/com/example/questionbook/room/Entity.kt
@@ -5,22 +5,47 @@ import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDate
+import java.time.LocalDateTime
 
+//カテゴリーテーブル
 @Entity(tableName = "question_category")
 data class QuestionCategory(
-    @PrimaryKey(autoGenerate = true) val categoryNo:Int,
-    @ColumnInfo(name = "category_title")var category:String
+    @PrimaryKey(autoGenerate = true)val categoryNo:Int,
+    @ColumnInfo(name = "category_title")var categoryTitle:String,
+    @ColumnInfo(name="category_flag")val categoryFlag:Int
 )
 
+//問題集テーブル
 @Entity(tableName = "question_workbook")
 data class QuestionWorkBook(
     @PrimaryKey(autoGenerate = true)val workBookNo:Int,
     @ColumnInfo(name = "workbook_title")val workBookTitle:String,
-    @ColumnInfo(name = "workbook_text")val workBookText:String,
-    @ColumnInfo(name = "workbook_accuracy")val answerAccuracyRate:Float,
+    @ColumnInfo(name = "workbook_flag")val workBookFlag:Int,
+    @ColumnInfo(name = "timestamp")val workBookDate:LocalDateTime,
     @ColumnInfo(name = "relation_category")val relationCategory:Int
 )
 
+//正解率テーブル
+@Entity(tableName = "question_accuracy")
+data class QuestionAccuracy(
+        @PrimaryKey(autoGenerate = true)val accuracyNo:Int,
+        @ColumnInfo(name = "accuracy_rate")val accuracyRate:Float,
+        @ColumnInfo(name = "accuracy_savedate")val accuracySaveDate:LocalDate,
+        @ColumnInfo(name = "relation_workbook")val relationWorkBook: Int
+)
+
+//問題文テーブル
+@Entity(tableName = "question_problem")
+data class QuestionProblem(
+    @PrimaryKey(autoGenerate = true)val problemNo:Int,
+    @ColumnInfo(name = "problem_statement")val problemStatement:String,
+    @ColumnInfo(name = "problem_flag")val problemFlag:Int,
+    @ColumnInfo(name = "timestamp")val timeStamp:LocalDateTime,
+    @ColumnInfo(name = "relation_workbook")val relationWorkBook: Int
+)
+
+//回答欄テーブル
 @Entity(tableName = "question_answer")
 data class QuestionAnswer(
     @PrimaryKey(autoGenerate = true)val answerNo:Int,
@@ -28,5 +53,5 @@ data class QuestionAnswer(
     @ColumnInfo(name = "answer_second")val answerSecond:String,
     @ColumnInfo(name = "answer_third")val answerThird:String,
     @ColumnInfo(name = "answer_right")val answerRight:String,
-    @ColumnInfo(name = "relation_workbook")val relationWorkBook:Int
+    @ColumnInfo(name = "relation_problem")val relationWorkBook:Int
 )
