cc6abd0f09fa7dd63ab88a5964ffcb57bc9732e3
yusk178 <youjiebupo@gmail.com>
2021-03-19 18:56:45 +0900

    データベースへ値を保存する際に、オブジェクト型をデータベースに指定することが不可能なためそのオブジェクト型をString型もしくは、プリミティブ型へ変換する必要がある。その型の変換を行うためのメソッドを追加しました。

--- app/src/main/java/com/example/questionbook/room/Converter.kt
+++ app/src/main/java/com/example/questionbook/room/Converter.kt
@@ -1,25 +1,32 @@
 package com.example.questionbook.room
 
 import androidx.room.TypeConverter
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class Converter {
     companion object{
-        const val pattern = "yyyy-MM-dd HH:mm"
+        const val DATETIME_PATTERN = "yyyy-MM-dd HH:mm"
+        const val LOCAL_PATTERN = "yyyy-MM-dd"
     }
     @TypeConverter
     fun fromTimestamp(dateStr:String?): LocalDateTime? {
         return dateStr?.let {
             LocalDateTime.parse(
-                dateStr, DateTimeFormatter.ofPattern(pattern)
+                dateStr, DateTimeFormatter.ofPattern(DATETIME_PATTERN)
             )
         }
     }
     @TypeConverter
     fun dateToTimestamp(date: LocalDateTime?): String? {
-        return date?.let {
-            it.format(DateTimeFormatter.ofPattern(pattern))
-        }
+        return date?.format(DateTimeFormatter.ofPattern(DATETIME_PATTERN))
     }
+    @TypeConverter
+    fun stringToLocalDate(dateStr: String?):LocalDate? =
+        LocalDate.parse(dateStr, DateTimeFormatter.ofPattern(LOCAL_PATTERN))
+
+    @TypeConverter
+    fun localDateToString(date:LocalDate?): String? =
+        date?.format(DateTimeFormatter.ofPattern(LOCAL_PATTERN))
 }
