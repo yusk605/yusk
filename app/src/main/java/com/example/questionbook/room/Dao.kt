8994aad5ffd3f26e67343d1c30119c28699ac12a
yusk178 <youjiebupo@gmail.com>
2021-03-19 19:28:07 +0900

    DB定義書の変更点を基に最低限必要となるメソッドの実装及び、新しく追加されたテーブルに基づくDaoの作成を行いました。

--- app/src/main/java/com/example/questionbook/room/Dao.kt
+++ app/src/main/java/com/example/questionbook/room/Dao.kt
@@ -1,51 +1,45 @@
 package com.example.questionbook.room
 
-import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
-@Dao
+@Dao    //カテゴリー
 interface QuestionCategoryDao{
-    @Query("select * from question_category")
-    fun getAll():LiveData<List<QuestionCategory>>
-
-    @Insert
-    suspend fun insert(entity:QuestionCategory)
-
-    @Update
-    suspend fun update(entity:QuestionCategory)
-
-    @Delete
-    suspend fun delete(entity:QuestionCategory)
-
+    @Query("select * from question_category") fun getAll():LiveData<List<QuestionCategoryEntity>>
+    @Insert suspend fun insert(entity:QuestionCategoryEntity)
+    @Update suspend fun update(entity:QuestionCategoryEntity)
+    @Delete suspend fun delete(entity:QuestionCategoryEntity)
 }
 
-@Dao
+@Dao    //問題集
 interface QuestionWorkBookDao{
-    @Query("select * from question_workbook")
-    fun getAll():LiveData<List<QuestionWorkBook>>
-
-    @Insert
-    suspend fun insert(entity:QuestionWorkBook)
-
-    @Update
-    suspend fun update(entity:QuestionWorkBook)
-
-    @Delete
-    suspend fun delete(entity:QuestionWorkBook)
+    @Query("select * from question_workbook")fun getAll():LiveData<List<QuestionWorkBookEntity>>
+    @Insert suspend fun insert(entity:QuestionWorkBookEntity)
+    @Update suspend fun update(entity:QuestionWorkBookEntity)
+    @Delete suspend fun delete(entity:QuestionWorkBookEntity)
 }
 
-@Dao
+@Dao    //問題文
+interface QuestionProblemDao{
+    @Query("select * from question_problem")fun getAll():LiveData<List<QuestionProblemEntity>>
+    @Insert suspend fun insert(entity:QuestionProblemEntity)
+    @Update suspend fun update(entity:QuestionProblemEntity)
+    @Delete suspend fun delete(entity:QuestionProblemEntity)
+
+}
+
+@Dao    //正解率
+interface QuestionAccuracyDao{
+    @Query("select * from question_answer")fun getAll():LiveData<List<QuestionAccuracyEntity>>
+    @Insert suspend fun insert(entity:QuestionAccuracyEntity)
+    @Update suspend fun update(entity:QuestionAccuracyEntity)
+    @Delete suspend fun delete(entity:QuestionAccuracyEntity)
+}
+
+@Dao    //回答欄
 interface QuestionAnswerDao{
-    @Query("select * from question_answer")
-    fun getAll():LiveData<List<QuestionAnswer>>
-
-    @Insert
-    fun insert(entity:QuestionAnswer)
-
-    @Update
-    fun update(entity:QuestionAnswer)
-
-    @Delete
-    fun delete(entity:QuestionAnswer)
+    @Query("select * from question_answer")fun getAll():LiveData<List<QuestionAnswerEntity>>
+    @Insert fun insert(entity:QuestionAnswerEntity)
+    @Update fun update(entity:QuestionAnswerEntity)
+    @Delete fun delete(entity:QuestionAnswerEntity)
 }
