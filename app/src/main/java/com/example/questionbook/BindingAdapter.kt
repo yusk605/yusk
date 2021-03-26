8d31da28dc36436898bde810196ee4152779e074
yusk178 <youjiebupo@gmail.com>
2021-03-26 19:44:56 +0900

    バインディングアダプターを使用する際の検討として。

--- app/src/main/java/com/example/questionbook/BindingAdapter.kt
+++ app/src/main/java/com/example/questionbook/BindingAdapter.kt
@@ -1,2 +1,14 @@
 package com.example.questionbook
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.questionbook.adapter.CategoryAdapter
+import com.example.questionbook.room.CategoryWithWorkBooks
+import com.example.questionbook.room.QuestionCategoryEntity
+import com.example.questionbook.view_model.CategoryViewModel
+
+
+@BindingAdapter("list")
+fun bindRecyclerView(recyclerView: RecyclerView) {
+
+}
