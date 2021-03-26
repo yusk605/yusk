85be2621dff0ffba83d12c8b95b0bcfa43a7d7e6
yusk178 <youjiebupo@gmail.com>
2021-03-26 19:52:23 +0900

    カテゴリフラグメントでの自動生成された不必要なコードを削除しました。

--- app/src/main/java/com/example/questionbook/fragment/CategoryFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/CategoryFragment.kt
@@ -5,57 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.questionbook.R
+import com.example.questionbook.adapter.CategoryAdapter
+import com.example.questionbook.databinding.FragmentCategoryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    lateinit var binding: FragmentCategoryBinding
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = CategoryAdapter { entity,view ->
+            //カテゴリーリストからアイテムをタップした場合に、
+            //カテゴリーに紐づいたワークブックをを表示させる画面へ遷移する。
+            Navigation.findNavController(view).navigate(
+                    R.id.action_categoryFragment_to_workBookFragment,
+                    Bundle().apply { putInt(ARGS_KEY,entity.categoryNo) }
+            )
+        }
     }
+
+    private fun recycleInit(){ }
+
+    companion object{
+        const val ARGS_KEY = "navigate_args_category_to_workBook"
+    }
+
 }
