900f5eb30f1e8441fa8f2c25f747a5a323d37ea9
yusk178 <youjiebupo@gmail.com>
2021-03-28 21:42:51 +0900

    workbook（問題集一覧画面）での必要なデータを表示させるためのViewModelを作成しました。

--- app/src/main/java/com/example/questionbook/view_model/WorkBookViewModel.kt
+++ app/src/main/java/com/example/questionbook/view_model/WorkBookViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.questionbook.view_model
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.questionbook.room.QuestionDatabase
 import com.example.questionbook.room.QuestionWorkBookDao
+import com.example.questionbook.room.QuestionWorkBookEntity
+import com.example.questionbook.room.WorkBookWithProblemsAndAccuracy
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class WorkBookViewModel(dao:QuestionWorkBookDao):ViewModel() {
+class WorkBookViewModel(app:Application):AndroidViewModel(app) {
+    private val  dao =
+        QuestionDatabase
+        .getInstance(app,viewModelScope as LifecycleCoroutineScope)
+        .getWorkBookDao()
+
+    val data:LiveData<List<WorkBookWithProblemsAndAccuracy>> by lazy {
+        dao.getAll()
+    }
+
+    fun insert(entity:QuestionWorkBookEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.insert(entity = entity)
+        }
+
+    fun update(entity: QuestionWorkBookEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.update(entity = entity)
+        }
+
+    fun delete(entity: QuestionWorkBookEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.update(entity = entity)
+        }
 }
