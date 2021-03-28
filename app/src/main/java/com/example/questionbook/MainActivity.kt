73d05881ac58a7a765e35cf65eb91f7958461bfe
yusk178 <youjiebupo@gmail.com>
2021-03-28 16:10:57 +0900

    ドロワービューを使用したサイドメニューの機能を実装させる多面メインアクシビィティ内にNavigationUiでの実行及び、ドロワーレイアウト、ナビゲーションコントローラーの生成を行った後にサイドメニューでタップされた項目に紐づくフラグメントに遷移を行う。

--- app/src/main/java/com/example/questionbook/MainActivity.kt
+++ app/src/main/java/com/example/questionbook/MainActivity.kt
@@ -6,15 +6,36 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.questionbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var controller: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        latainit()
         binding = setContentView<ActivityMainBinding>(this,R.layout.activity_main)
-
+        NavigationUI.setupActionBarWithNavController(this,controller,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navigationView,controller)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(controller,drawerLayout)
+    }
+
+    /**
+     * バッキングフィールドへ初期化を行うメソッド。
+     */
+    private fun latainit(){
+        drawerLayout = binding.drawerLayout
+        controller = this.findNavController(R.id.nav_controller_view_tag)
+    }
+
 }
