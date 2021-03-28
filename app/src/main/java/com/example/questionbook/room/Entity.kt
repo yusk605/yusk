e07715cc3c3783dfe8135c90f90ecb0fa0e92f7d
yusk178 <youjiebupo@gmail.com>
2021-03-28 21:49:05 +0900

    フラグメント内での値の受け渡しを行うためにオブジェクトをシリアライザブル化させる必要があるためパーライザブルを実装させました。

--- app/src/main/java/com/example/questionbook/room/Entity.kt
+++ app/src/main/java/com/example/questionbook/room/Entity.kt
@@ -3,18 +3,22 @@ package com.example.questionbook.room
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.*
+import kotlinx.android.parcel.Parceler
+import kotlinx.android.parcel.Parcelize
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 //カテゴリーテーブル
+@Parcelize
 @Entity(tableName = "question_category")
 data class QuestionCategoryEntity(
     @PrimaryKey(autoGenerate = true)val categoryNo:Int,
     @ColumnInfo(name = "category_title")var categoryTitle:String,
     @ColumnInfo(name="category_flag")val categoryFlag:Int
-)
+):Parcelable
 
 //問題集テーブル
+@Parcelize
 @Entity(tableName = "question_workbook")
 data class QuestionWorkBookEntity(
     @PrimaryKey(autoGenerate = true)val workBookNo:Int,
@@ -22,7 +26,7 @@ data class QuestionWorkBookEntity(
     @ColumnInfo(name = "workbook_flag")val workBookFlag:Int,
     @ColumnInfo(name = "timestamp")val workBookDate:LocalDateTime,
     @ColumnInfo(name = "relation_category")val relationCategory:Int
-)
+):Parcelable
 
 //正解率テーブル
 @Entity(tableName = "question_accuracy")
@@ -34,6 +38,7 @@ data class QuestionAccuracyEntity(
 )
 
 //問題文テーブル
+@Parcelize
 @Entity(tableName = "question_problem")
 data class QuestionProblemEntity(
     @PrimaryKey(autoGenerate = true)val problemNo:Int,
@@ -41,9 +46,10 @@ data class QuestionProblemEntity(
     @ColumnInfo(name = "problem_flag")val problemFlag:Int,
     @ColumnInfo(name = "timestamp")val timeStamp:LocalDateTime,
     @ColumnInfo(name = "relation_workbook")val relationWorkBook: Int
-)
+):Parcelable
 
 //回答欄テーブル
+@Parcelize
 @Entity(tableName = "question_answer")
 data class QuestionAnswerEntity(
     @PrimaryKey(autoGenerate = true)val answerNo:Int,
@@ -52,12 +58,12 @@ data class QuestionAnswerEntity(
     @ColumnInfo(name = "answer_third")val answerThird:String,
     @ColumnInfo(name = "answer_right")val answerRight:String,
     @ColumnInfo(name = "relation_problem")val relationWorkBook:Int
-)
-
+):Parcelable
 
 /**
  * カテゴリーテーブルの識別番号に紐づいた問題集一覧を取得します。
  */
+@Parcelize
 data class CategoryWithWorkBooks(
         @Embedded
         val questionCategoryEntity:QuestionCategoryEntity,
@@ -65,10 +71,12 @@ data class CategoryWithWorkBooks(
                 parentColumn = "categoryNo",
                 entityColumn = "relation_category"
         )val workBookList:List<QuestionWorkBookEntity>
-)
+):Parcelable
+
 /**
  * ワークブック（問題集）の識別番号に紐づいた、テキスト（問題）をすべて取得する。
  */
+@Parcelize
 data class WorkBookWithProblemsAndAccuracy(
         @Embedded
         val workBookEntity:QuestionWorkBookEntity,
@@ -80,11 +88,12 @@ data class WorkBookWithProblemsAndAccuracy(
                 parentColumn = "workBookNo",
                 entityColumn = "relation_workbook")
         val accuracyList:List<QuestionAccuracyEntity>
-)
+):Parcelable
 
 /**
  *問題となるテキストの識別番号に紐づいた、回答欄すべてを取得する。
  */
+@Parcelize
 data class ProblemWithAnswer(
         @Embedded
         val problemEntity: QuestionProblemEntity,
@@ -92,4 +101,4 @@ data class ProblemWithAnswer(
                 parentColumn = "problemNo",
                 entityColumn = "relation_problem"
         )val answerList:List<QuestionAnswerEntity>
-)
+):Parcelable
