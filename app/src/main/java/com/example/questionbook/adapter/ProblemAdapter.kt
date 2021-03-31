adfe02413341ffeb5b710366086eb6e39ede1807
yusk178 <youjiebupo@gmail.com>
2021-03-31 13:30:33 +0900

    アダプタークラス内でのホルダークラスを生成するために必要なメソッドの中身を実装しました。

--- app/src/main/java/com/example/questionbook/adapter/ProblemAdapter.kt
+++ app/src/main/java/com/example/questionbook/adapter/ProblemAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.questionbook.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.questionbook.R
 import com.example.questionbook.room.ProblemWithAnswer
+import com.example.questionbook.room.QuestionProblemEntity
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_problem.view.*
 
-class ProblemAdapter:ListAdapter<ProblemWithAnswer,ProblemAdapter.ProblemHolder>(Diff) {
+class ProblemAdapter(private val onClick:(QuestionProblemEntity)->Unit):ListAdapter<ProblemWithAnswer,ProblemAdapter.ProblemHolder>(Diff) {
 
     companion object Diff:DiffUtil.ItemCallback<ProblemWithAnswer>(){
         override fun areItemsTheSame(
@@ -22,13 +28,25 @@ class ProblemAdapter:ListAdapter<ProblemWithAnswer,ProblemAdapter.ProblemHolder>
 
     }
     inner class ProblemHolder(view:View):RecyclerView.ViewHolder(view){
-
+        val statementEditText = view.findViewById<TextInputEditText>(R.id.problem_statement_edit_text)
+        val radioGroup = view.findViewById<RadioGroup>(R.id.problem_radio_group)
+        //val problem_counter =
+        init {
+            onClick(
+                getItem(layoutPosition).problemEntity
+            )
+        }
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProblemHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProblemHolder =
+        ProblemHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.fragment_problem,parent,false)
+        )
 
     override fun onBindViewHolder(holder: ProblemHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.also {
+           it.statementEditText.setText(
+               getItem(position).problemEntity.problemStatement
+           )
+       }
     }
 }
