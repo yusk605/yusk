9d72a695645322415332e44b58aedfacda779312
yusk178 <youjiebupo@gmail.com>
2021-03-31 16:32:57 +0900

    ラジオボタンを変更する際に必要なメソッドを実装しました。

--- app/src/main/java/com/example/questionbook/adapter/ProblemAdapter.kt
+++ app/src/main/java/com/example/questionbook/adapter/ProblemAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -13,9 +14,12 @@ import com.example.questionbook.room.QuestionProblemEntity
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.fragment_problem.view.*
 
-class ProblemAdapter(private val onClick:(QuestionProblemEntity)->Unit):ListAdapter<ProblemWithAnswer,ProblemAdapter.ProblemHolder>(Diff) {
+class ProblemAdapter(
+    private val onClick:(QuestionProblemEntity)->Unit
+):ListAdapter<ProblemWithAnswer,ProblemAdapter.ProblemHolder>(Diff) {
 
     companion object Diff:DiffUtil.ItemCallback<ProblemWithAnswer>(){
+
         override fun areItemsTheSame(
             oldItem: ProblemWithAnswer,
             newItem: ProblemWithAnswer
@@ -27,16 +31,20 @@ class ProblemAdapter(private val onClick:(QuestionProblemEntity)->Unit):ListAdap
         ): Boolean = oldItem == newItem
 
     }
+
     inner class ProblemHolder(view:View):RecyclerView.ViewHolder(view){
-        val statementEditText = view.findViewById<TextInputEditText>(R.id.problem_statement_edit_text)
+        val statementEditText = view.findViewById<TextInputEditText>(R.id.item_problem_statement_edit)
         val radioGroup = view.findViewById<RadioGroup>(R.id.problem_radio_group)
-        //val problem_counter =
+        val problem_counter = view.findViewById<TextView>(R.id.item_problem_counter)
+
         init {
             onClick(
                 getItem(layoutPosition).problemEntity
             )
         }
+
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProblemHolder =
         ProblemHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.fragment_problem,parent,false)
@@ -46,7 +54,16 @@ class ProblemAdapter(private val onClick:(QuestionProblemEntity)->Unit):ListAdap
        holder.also {
            it.statementEditText.setText(
                getItem(position).problemEntity.problemStatement
-           )
+            )
+           it.radioGroup.checkedRadioButtonId
        }
     }
+
+    private fun Int.toConverterRadioButtonId() =
+        when(this){
+            1 -> R.id.problem_radio_button_first
+            2 -> R.id.problem_radio_button_second
+            3 -> R.id.problem_radio_button_third
+            else -> R.id.problem_radio_button_fourth
+        }
 }
