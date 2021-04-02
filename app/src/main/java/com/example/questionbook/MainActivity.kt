b7f817e69be2261d8fbfa593d82a28198d70f5ca
yusk178 <youjiebupo@gmail.com>
2021-04-02 22:42:21 +0900

    改行を増やしました

--- app/src/main/java/com/example/questionbook/MainActivity.kt
+++ app/src/main/java/com/example/questionbook/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(binding.navigationView,controller)
     }
 
+
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(controller,drawerLayout)
     }
