55b435a5c313d5425c1db863d3ded9dbf8a8bafd
yusk178 <youjiebupo@gmail.com>
2021-04-05 20:45:58 +0900

    ラボラトリーでのフラグメントを作成しました

--- app/src/main/java/com/example/questionbook/fragment/LaboratoryFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/LaboratoryFragment.kt
@@ -5,25 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.questionbook.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+import kotlinx.android.synthetic.main.fragment_laboratory.*
 
 class LaboratoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    //TODO ViewModel（Category,WorkBook）を作成を行うこと。
+    //TODO ダイヤログの作成を行うこと。
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,23 +26,17 @@ class LaboratoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_laboratory, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LaboratoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LaboratoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        laboratory_register_category.setOnClickListener {  }
+        laboratory_register_workbook.setOnClickListener {  }
+
+        laboratory_register_problem.setOnClickListener {
+            Navigation.findNavController(it).navigate(
+                R.id.action_laboratoryFragment_to_problemInsertFragment
+            )
+        }
+
     }
 }
