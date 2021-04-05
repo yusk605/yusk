c2948300105c87aec25f694cbbec8fe579588a6d
yusk178 <youjiebupo@gmail.com>
2021-04-05 23:06:38 +0900

    問題集をデータベースに追加するアルゴリズムを作成途中

--- app/src/main/java/com/example/questionbook/fragment/QuestionFormFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/QuestionFormFragment.kt
@@ -5,24 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.questionbook.R
 import com.example.questionbook.room.QuestionProblemEntity
-import com.example.questionbook.view_model.QuestionFormViewModel
-import com.example.questionbook.view_model.QuestionViewModelFactory
+import com.example.questionbook.view_model.*
 import kotlinx.android.synthetic.main.fragment_problem_insert.*
 import java.time.LocalDateTime
 
 class QuestionFormFragment : Fragment() {
 
+    private val app = activity?.application
+
     private val formViewModel:QuestionFormViewModel by lazy {
-        QuestionViewModelFactory(app = activity?.application!!)
+        QuestionViewModelFactory(app = app!!)
             .create(QuestionFormViewModel::class.java)
+    }
+
+    private val categoryViewModel:CategoryViewModel by lazy {
+        CategoryViewModelFactory(app = app!!)
+            .create(CategoryViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,10 +43,48 @@ class QuestionFormFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        categoryViewModel.data.observe(viewLifecycleOwner)
+        { data ->
+            form_problem_category_spinner.adapter =
+                activity?.let {fragmentActivity->
+                    ArrayAdapter(
+                        fragmentActivity,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        data.map { it.questionCategoryEntity }
+                            .map { it.categoryTitle }
+                            .toList() as Array<out String>
+                    ) }
+
+        }
+
         form_problem_btn.setOnClickListener {
 
         }
     }
+
+    private fun get(){
+        form_problem_category_spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val sp   = parent as? Spinner
+                val item = sp?.selectedItem as? String
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    private fun setSpinnerItem(){
+        form_problem_workbook_spinner.adapter
+    }
+
     private fun setProblemEntity() = QuestionProblemEntity(
         problemNo = 0,
         problemFlag = 0,
