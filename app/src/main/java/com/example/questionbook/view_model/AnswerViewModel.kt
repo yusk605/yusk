fc2c4dd2030610e212ff41b5abce572fdd71a96c
yusk178 <youjiebupo@gmail.com>
2021-04-05 18:13:50 +0900

    ビューモデルの生成を行いました

--- app/src/main/java/com/example/questionbook/view_model/AnswerViewModel.kt
+++ app/src/main/java/com/example/questionbook/view_model/AnswerViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.questionbook.view_model
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 import com.example.questionbook.room.QuestionAnswerDao
+import com.example.questionbook.room.QuestionAnswerEntity
+import com.example.questionbook.room.QuestionDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AnswerViewModel(private val dao:QuestionAnswerDao): ViewModel() {
+class AnswerViewModel(private val app:Application): AndroidViewModel(app) {
+    private val db   = QuestionDatabase.getInstance(app,viewModelScope as LifecycleCoroutineScope)
+    private val dao  = db.getAnswerDao()
+    private var _data = dao.getAll()
 
+    val data:LiveData<List<QuestionAnswerEntity>>
+        get() = _data
+
+    fun insert(entity:QuestionAnswerEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.insert(entity = entity)
+        }
+
+    fun update(entity: QuestionAnswerEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.update(entity = entity)
+        }
+
+    fun delete(entity: QuestionAnswerEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.delete(entity = entity)
+        }
 }
