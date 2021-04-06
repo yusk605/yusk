cbe5e6830feb53933f2d321fb70d1e7957005f27
yusk178 <youjiebupo@gmail.com>
2021-04-06 18:48:59 +0900

    ウィジェットでの共通で使用する関数を作成

--- app/src/main/java/com/example/questionbook/ExtWidget.kt
+++ app/src/main/java/com/example/questionbook/ExtWidget.kt
@@ -1,2 +1,19 @@
 package com.example.questionbook
 
+import android.content.Context
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+/**
+ * ■スピナーでのドロップリストに保存したい値を入れる
+ * @param data ドロップリストに表示させたい文字列
+ * @param context コンテキストルート
+ */
+fun Spinner.setAdapter(data:List<String>,context: Context){
+    adapter = ArrayAdapter(
+        context,
+        android.R.layout.simple_spinner_dropdown_item,
+        data
+    )
+}
+
