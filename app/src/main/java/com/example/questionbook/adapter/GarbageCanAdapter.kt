6d41e9e1cd6c99908ac9f187758736f836064f50
yusk178 <youjiebupo@gmail.com>
2021-04-08 22:33:08 +0900

    削除を確定させる画面を表示させるためのクラス。

--- app/src/main/java/com/example/questionbook/adapter/GarbageCanAdapter.kt
+++ app/src/main/java/com/example/questionbook/adapter/GarbageCanAdapter.kt
@@ -1,4 +1,12 @@
 package com.example.questionbook.adapter
 
-class GarbageCanAdapter {
+import android.media.browse.MediaBrowser
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.questionbook.room.QuestionCategoryEntity
+
+class GarbageCanAdapter{
+
 }
