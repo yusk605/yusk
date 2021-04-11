99b07f7ced1be5195475980dd577f6346fd51926
yusk178 <youjiebupo@gmail.com>
2021-04-11 22:22:07 +0900

    カテゴリー一覧での必要な処理を実装しました。

--- app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
+++ app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
@@ -1,4 +1,26 @@
 package com.example.questionbook.dialog
 
-class CategoryDialog {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.fragment.app.DialogFragment
+import com.example.questionbook.R
+
+class CategoryDialog:DialogFragment() {
+
+    private val dialog:AlertDialog.Builder = AlertDialog.Builder(activity)
+    private val layout = activity?.layoutInflater?.inflate(
+        R.layout.dialog_category_layout,null ) as LinearLayout
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return dialog.also {
+            it.setTitle(getString(R.string.dialog_category_title))
+            it.setIcon(R.drawable.question_book_holder)
+            it.setView(layout)
+        }.create()
+    }
+
+    //TODO カスタムダイアログレイアウトでの値を取得するウィジェットの処理を記述すること。
+
 }
