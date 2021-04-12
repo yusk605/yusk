60c0bf8b4e061abb6af7d96e685dafa9d3ed067b
yusk178 <youjiebupo@gmail.com>
2021-04-12 20:28:36 +0900

    ダイヤログを表示させるためのクラス及びファクトリーメソッドの作成を行いました。

--- app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
+++ app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
@@ -3,24 +3,33 @@ package com.example.questionbook.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
 import com.example.questionbook.R
 
-class CategoryDialog:DialogFragment() {
+class CategoryDialog private constructor(
+        private val activity: FragmentActivity,
+        @LayoutRes private val resource:Int
+    ):DialogProduct(activity) {
 
-    private val dialog:AlertDialog.Builder = AlertDialog.Builder(activity)
-    private val layout = activity?.layoutInflater?.inflate(
-        R.layout.dialog_category_layout,null ) as LinearLayout
+    private val dialogView:View by lazy { activity.layoutInflater.inflate(resource,null) }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return dialog.also {
-            it.setTitle(getString(R.string.dialog_category_title))
-            it.setIcon(R.drawable.question_book_holder)
-            it.setView(layout)
-        }.create()
+    override fun create(): AlertDialog =
+            super.dialog.also {
+                it.setTitle(activity.getString(R.string.dialog_category_title))
+                it.setView(dialogView as LinearLayout)
+            }.create()
+
+    override fun getView(): View = dialogView
+
+    companion object{
+            private var singleton:CategoryDialog? = null
+            fun getInstance(
+                    activity: FragmentActivity,
+                    @LayoutRes resource:Int
+            ):CategoryDialog = singleton?:CategoryDialog(activity, resource)
+        }
     }
-
-    //TODO カスタムダイアログレイアウトでの値を取得するウィジェットの処理を記述すること。
-
-}
--- app/src/main/java/com/example/questionbook/dialog/DialogFactoryMethod.kt
+++ app/src/main/java/com/example/questionbook/dialog/DialogFactoryMethod.kt
@@ -7,36 +7,42 @@ import androidx.annotation.LayoutRes
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
 
+/**
+ * 抽象的なファクトリーとなるインターフェイス。
+ */
 interface DialogFactoryImpl<T:DialogProduct>{
     fun create(dialogClass:Class<T>):DialogProduct
 }
 
+/**
+ * 抽象的なダイヤログクラス
+ * @param activity  フラグメント内で取得することが出来るコンテキスト
+ * @param resource  表示させたいレイアウトファイル 例：R.layout.....xml
+ */
 abstract class DialogProduct(
-        private val activity: FragmentActivity,
-        @LayoutRes private val resource:Int
+        private val activity: FragmentActivity
     ){
 
     private val _dialog: AlertDialog.Builder by lazy { AlertDialog.Builder(activity) }
-    private val _view:View by lazy { activity.layoutInflater.inflate(resource,null) }
 
-    val view:View
-        get() = _view
-
-    val dialog: AlertDialog.Builder
+    protected val dialog: AlertDialog.Builder
         get() = _dialog
 
     abstract fun create():AlertDialog
+    abstract fun getView():View
 }
 
 
-
-class CategoryDialogFactory(
+/**
+ *
+ */
+class CategoryDialogFactory (
         private val activity: FragmentActivity,
         @LayoutRes private val resource:Int
 ):DialogFactoryImpl<CategoryDialog>{
     override fun create(dialogClass: Class<CategoryDialog>):DialogProduct {
         if (dialogClass.isAssignableFrom(CategoryDialog::class.java))
-            return CategoryDialog(activity,resource)
+            return CategoryDialog.getInstance(activity,resource)
 
         throw IllegalArgumentException("Type mismatch CategoryDialog")
     }
