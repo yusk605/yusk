42d232ef1efcad01d5ed6ef0e1554781333e6110
yusk178 <youjiebupo@gmail.com>
2021-04-12 21:42:56 +0900

    問題集を追加するためのフラグメントを作成

--- app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
+++ app/src/main/java/com/example/questionbook/dialog/CategoryDialog.kt
@@ -1,12 +1,10 @@
 package com.example.questionbook.dialog
 
 import android.app.AlertDialog
-import android.app.Dialog
-import android.os.Bundle
+
 import android.view.View
 import android.widget.LinearLayout
 import androidx.annotation.LayoutRes
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
 import com.example.questionbook.R
 
@@ -20,16 +18,13 @@ class CategoryDialog private constructor(
     override fun create(): AlertDialog =
             super.dialog.also {
                 it.setTitle(activity.getString(R.string.dialog_category_title))
-                it.setView(dialogView as LinearLayout)
-            }.create()
+                it.setView(dialogView as LinearLayout) }.create()
 
-    override fun getView(): View = dialogView
+    override fun getView():View = dialogView
 
     companion object{
             private var singleton:CategoryDialog? = null
-            fun getInstance(
-                    activity: FragmentActivity,
-                    @LayoutRes resource:Int
-            ):CategoryDialog = singleton?:CategoryDialog(activity, resource)
+            fun getInstance(activity: FragmentActivity, @LayoutRes resource:Int): CategoryDialog =
+                    singleton?:CategoryDialog(activity, resource)
         }
     }
---
+++ app/src/main/java/com/example/questionbook/dialog/WorkBookDialog.kt
@@ -0,0 +1,34 @@
+package com.example.questionbook.dialog
+
+import android.app.AlertDialog
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.FragmentActivity
+import com.example.questionbook.R
+
+class WorkBookDialog(
+        private val activity: FragmentActivity,
+        @LayoutRes private val resource:Int
+    ):DialogProduct(activity) {
+
+    private val dialogView:View by lazy {
+        activity.layoutInflater.inflate(resource,null)
+    }
+
+    override fun create(): AlertDialog =
+            super.dialog.also {
+                it.setTitle(activity.getString(R.string.dialog_workbook_title))
+                it.setView(dialogView as LinearLayout)
+            }.create()
+
+    override fun getView(): View {
+        return dialogView
+    }
+    companion object{
+        var singleton:WorkBookDialog? =null
+        fun getInstance(activity: FragmentActivity,@LayoutRes resource:Int):WorkBookDialog =
+            singleton?:WorkBookDialog(activity,resource)
+    }
+
+}
