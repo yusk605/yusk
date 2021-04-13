a0c7f4d620392e459ac825ef1f4cdb7fdb57f3f9
yusk178 <youjiebupo@gmail.com>
2021-04-14 08:17:55 +0900

    問題を表示させるための値を格納するためのオブジェクト

--- app/src/main/java/com/example/questionbook/Model.kt
+++ app/src/main/java/com/example/questionbook/Model.kt
@@ -1,2 +1,22 @@
 package com.example.questionbook
 
+/**
+ * ■問題を表示させるためのオブジェクト
+ * @param questionTitle 問題となるタイトル
+ * @param questionStatement 問題文
+ * @param questionFirs   解答案1
+ * @param questionSecond 解答案2
+ * @param questionThird　解答案3
+ * @param questionRight  正解
+ */
+data class QuestionItem(
+        val questionStatement:String,
+        val questionTitle:String,
+        val questionFirs:String,
+        val questionSecond:String,
+        val questionThird:String,
+        val questionRight:String,
+        )
+/*
+    @Note　問題に必要な値を取得するものを考える。
+ */
