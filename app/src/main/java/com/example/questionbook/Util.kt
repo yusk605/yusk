c8e3c507b75cdfbd376d30d2ba478cb35070a085
yusk178 <youjiebupo@gmail.com>
2021-04-17 12:12:01 +0900

    項目を選択した時にスナックバーでの表示を行いました。

--- app/src/main/java/com/example/questionbook/Util.kt
+++ app/src/main/java/com/example/questionbook/Util.kt
@@ -1,8 +1,11 @@
 package com.example.questionbook
 
 import android.content.Context
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * ■スピナーでのドロップリストに保存したい値を入れる
@@ -16,4 +19,19 @@ fun Spinner.setAdapter(data:List<String>,context: Context){
         data
     )
 }
+
+/**
+ * ■スナックバーを表示させるためのメソッド
+ * @param msg スナックバーの文字列を表示させるため
+ */
+fun View.showSnackBar(msg:String){
+    Snackbar.make(this,msg, Snackbar.LENGTH_SHORT).show()
+}
+
+fun FragmentActivity.getMag(no:Int):String =
+        when (no){
+            0 -> getString(R.string.menu_side_holder)
+            1 -> getString(R.string.menu_side_game)
+            else -> getString(R.string.menu_side_accuracy)
+        }
 
