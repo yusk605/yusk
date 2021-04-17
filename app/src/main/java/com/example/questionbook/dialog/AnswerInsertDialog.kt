637c00c4a179f67ef12ef682fa701c42b0b2a388
yusk178 <youjiebupo@gmail.com>
2021-04-17 12:11:06 +0900

    回答欄を表示させるダイヤログクラスを作成しました。

--- app/src/main/java/com/example/questionbook/dialog/AnswerInsertDialog.kt
+++ app/src/main/java/com/example/questionbook/dialog/AnswerInsertDialog.kt
@@ -1,4 +1,42 @@
 package com.example.questionbook.dialog
 
-class AnswerInsertDialog {
+import android.app.Activity
+import android.app.AlertDialog
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.FragmentActivity
+import com.example.questionbook.R
+
+class AnswerInsertDialog(
+        private val activity: FragmentActivity,
+        @LayoutRes private val resource:Int
+        ):DialogProduct(activity = activity) {
+
+    private val view = activity.layoutInflater.inflate(resource,null)
+
+    override fun create(): AlertDialog {
+        return super.dialog
+                .setTitle(activity.getString(R.string.dialog_text_title))
+                .setNegativeButton(activity.getString(R.string.dialog_negative_button)){
+                    dialog, which ->
+                    dialog.cancel()
+                    }
+                .create()
+    }
+
+    override fun getView(): View {
+        return  view
+    }
+
+    override fun getValue(): String {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        private var  singleton:AnswerInsertDialog? =null
+        fun getInstance(
+                activity: FragmentActivity,
+                resource:Int
+        ) = singleton?:AnswerInsertDialog(activity, resource)
+    }
 }
