26a4fd96f57a0bc5af55fe2d162e6be68fc06e3b
yusk178 <youjiebupo@gmail.com>
2021-04-20 21:54:12 +0900

    不要なプロパティの削除を行いました。

--- app/src/main/java/com/example/questionbook/Model.kt
+++ app/src/main/java/com/example/questionbook/Model.kt
@@ -4,22 +4,12 @@ import com.example.questionbook.room.QuestionQuizEntity
 
 /**
  * ■問題を表示させるためのオブジェクト
- * @param questionTitle 問題となるタイトル
- * @param questionStatement 問題文
- * @param questionFirs   解答案1
- * @param questionSecond 解答案2
- * @param questionThird　解答案3
- * @param questionRight  正解
+ * @param entity        クイズを格納するためのエンティティ。
  * @param answerCheck   0..デフォルトの値 1..正解となる値 2..不正解となる値
  * @param selectAnswers クイズゲームを行う際に選択案をシャッフルさせるためのリスト。
  */
 data class QuestionItem(
-        val questionStatement:String,
-        val questionTitle:String,
-        val questionFirs:String,
-        val questionSecond:String,
-        val questionThird:String,
-        val questionRight:String,
+        var questionTitle:String,
         var answerCheck:Int,
         val entity: QuestionQuizEntity,
         val selectAnswers:MutableList<String>
