633f870823ce5b4603387a6191de0439e91c1172
yusk178 <youjiebupo@gmail.com>
2021-04-24 20:44:55 +0900

    クイズゲームを行った際のリザルトを表示させる。

--- app/src/main/java/com/example/questionbook/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/ResultFragment.kt
@@ -5,33 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.questionbook.QuizResult
 import com.example.questionbook.R
-import com.example.questionbook.ResultItem
+import kotlinx.android.synthetic.main.fragment_result.*
 
 class ResultFragment : Fragment() {
 
     //クイズゲームを行った時に渡される値
-    private lateinit var resultItem:ResultItem
+    private lateinit var resultItem:QuizResult
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            resultItem = it.get(GameStartFragment.ARGS_KEY) as ResultItem
+            resultItem = it.get(GameStartFragment.ARGS_KEY) as QuizResult
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        resultItem?.let { it.set() }
+
+    }
+
+    private fun QuizResult.set(){
+        fragment_result_title_text.text = resultTitle
+        result_result_text.text         = resultText
+        result_progress_bar.progress    = resultProgress
+        result_progress_accuracy.text   = "${resultAccuracy.toInt()}%"
     }
 
     companion object{
         const val SAFE_ARGS_KEY = "ResultFragment to ConcreteResultFragment"
     }
-
 }
