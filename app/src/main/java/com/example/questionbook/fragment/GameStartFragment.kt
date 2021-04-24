3333efd91d37843eb065b1e827e7b93e49dadeb5
yusk178 <youjiebupo@gmail.com>
2021-04-24 20:29:44 +0900

    クイズゲームを行うためのロジックを修正

--- app/src/main/java/com/example/questionbook/fragment/GameStartFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/GameStartFragment.kt
@@ -8,8 +8,8 @@ import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.navigation.Navigation
 import com.example.questionbook.QuestionItem
+import com.example.questionbook.QuizResult
 import com.example.questionbook.R
-import com.example.questionbook.ResultItem
 import com.example.questionbook.databinding.FragmentGameStartBinding
 import com.example.questionbook.logic.QuestionItemIterator
 import com.example.questionbook.logic.QuestionItemShelf
@@ -25,6 +25,7 @@ class GameStartFragment : Fragment() {
     private var questionIt:QuestionItemIterator? = null
     private var questionItemShelf:QuestionItemShelf? = null
     private var questionItem:QuestionItem? = null
+    private var radioButton:RadioButton? = null
 
     private var questionItemList:MutableList<QuestionItem> = mutableListOf()
 
@@ -55,29 +56,41 @@ class GameStartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
-
+            var selectAnswer = ""
             viewModel.data.observe(viewLifecycleOwner){
                 data->
-                questionItemShelf = QuestionItemShelf(data, workBookWithAll?.workBookEntity?.workBookTitle?:"")
+                questionItemShelf = QuestionItemShelf(
+                        data.filter { it.relationWorkBook == workBookWithAll?.workBookEntity?.workBookNo },
+                        workBookWithAll?.workBookEntity?.workBookTitle?:""
+                    )
                 questionIt = questionItemShelf?.let{ it.createIterator() }
                 questionIt?.let {
                     questionItem = it.next()
                     questionItem?.set()
+                    questionItemList.add(questionItem!!)
                     game_start_quiz_count.text = "${it.getIndex()}/${it.getSize()}"
                 }
-            }
 
-            //次のボタンを押したときの処理
-            game_answer_btn.setOnClickListener { view ->
-                var select = getSelectAnswer()
-                questionItemShelf?.let { shelf->
-                    questionItem?.let { q->
-                        shelf.incorrectAnswerCount(q,select)
-                        shelf.correctAnswerCount(q,select)
-                    } }
-                questionIt?.let{
-                    it.nextQuiz(view)
-                    game_start_quiz_count.text = "${it.getIndex()}/${it.getSize()}"
+
+                game_radio_group.setOnCheckedChangeListener {
+                    group, checkedId ->
+                    radioButton = group.findViewById<RadioButton>(checkedId)
+                    selectAnswer = radioButton?.text.toString()
+                }
+
+                //次のボタンを押したときの処理
+                game_answer_btn.setOnClickListener { view ->
+                    var select = selectAnswer
+                    game_radio_group.clearCheck()  //ラジオボタンにチェックを外すメソッド
+                    questionItemShelf?.let { shelf->
+                        questionItem?.let { q->
+                            shelf.incorrectAnswerCount(q,select)
+                            shelf.correctAnswerCount(q,select)
+                        } }
+                    questionIt?.let{
+                        it.nextQuiz(view)
+                        game_start_quiz_count.text = "${it.getIndex()}/${it.getSize()}"
+                    }
                 }
             }
         }
@@ -91,21 +104,33 @@ class GameStartFragment : Fragment() {
      */
     private fun QuestionItemIterator.nextQuiz(view: View){
         if(hasNext()) {
-            questionItem = questionIt?.next()
+            questionItem = next()
             questionItem?.let {
                 it.set()
                 questionItemList.add(it)
             }
-
         }else{
-
-            val bundle = Bundle().apply { putParcelable(ARGS_KEY,ResultItem(questionItemList)) }
-
-            Navigation.findNavController(view)
-                    .navigate(R.id.action_gameStartFragment_to_resultFragment,bundle)
+            val bundle = Bundle().apply { putParcelable(ARGS_KEY,questionItem?.setResult()) }
+            Navigation.findNavController(view).navigate(R.id.action_gameStartFragment_to_resultFragment,bundle)
         }
     }
 
+    private fun QuestionItem.setResult():QuizResult{
+        val quizCount           = questionItemList.size
+        val quizAnswerCount     = questionItemList.filter { it.answerCheck==1 }.size
+        val quizAccuracy:Float  = (quizAnswerCount.toFloat()/quizCount.toFloat())*100
+        return QuizResult(
+                resultTitle = questionTitle,
+                resultText = "${quizCount}問中${quizAnswerCount}問正解",
+                resultAccuracy = quizAccuracy,
+                resultProgress = quizAccuracy.toInt()
+        )
+    }
+
+    /**
+     * ■ラジオボタンから選択した文字列を返す
+     * @return 文字列
+     */
     private fun getSelectAnswer():String{
         var selectAnswer = ""
         game_radio_group.setOnCheckedChangeListener {
@@ -116,8 +141,11 @@ class GameStartFragment : Fragment() {
         return selectAnswer
     }
 
+    /**
+     * ■パラメーターをダイヤログに値を持たせる。
+     */
     private fun QuestionItem.set(){
-        game_start_statement_edit.setText(entity.quizStatement)
+        item_list_quiz_statement_edit.setText(entity.quizStatement)
         game_radio_button_first.text    = selectAnswers[0]
         game_radio_button_second.text   = selectAnswers[1]
         game_radio_button_third.text    = selectAnswers[2]
