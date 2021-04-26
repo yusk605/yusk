e5529603297dc2d8e3114befcaf583e8d6724701
yusk178 <youjiebupo@gmail.com>
2021-04-26 19:39:46 +0900

    リザルトを表示させるためのロジック。具体的には、リサイクラービューの初期処理を行うメソッドをコードで記述

--- app/src/main/java/com/example/questionbook/fragment/ConcreteResultListFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/ConcreteResultListFragment.kt
@@ -5,21 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.questionbook.R
+import kotlinx.android.synthetic.main.fragment_concrete_result_list.*
 
 
 class ConcreteResultListFragment : Fragment() {
 
+    private var workBookNo:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            workBookNo = it.getInt(ResultFragment.SAFE_ARGS_KEY)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_concrete_result_list, container, false)
+    }
+
+    private fun recycleInit(){
+        recyclerview_concrete_result.also {
+            recyclerView ->
+            recyclerView.adapter
+            recyclerView.layoutManager =
+                    LinearLayoutManager(activity).apply { orientation == LinearLayoutManager.VERTICAL }
+
+        }
     }
 
     companion object {
