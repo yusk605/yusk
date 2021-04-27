b2a0c2f8646eeedd0d3b1f196d29e227e0404040
yusk178 <youjiebupo@gmail.com>
2021-04-27 16:09:29 +0900

    リザルトを表示した時の詳細となる一覧の履歴を表示させるためのロジックを記述

--- app/src/main/java/com/example/questionbook/fragment/ConcreteResultListFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/ConcreteResultListFragment.kt
@@ -6,34 +6,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.questionbook.QuizResult
 import com.example.questionbook.R
+import com.example.questionbook.adapter.ConcreteResultListAdapter
+import com.example.questionbook.view_model.ResultViewModel
+import com.example.questionbook.view_model.ResultViewModelFactory
 import kotlinx.android.synthetic.main.fragment_concrete_result_list.*
 
 
 class ConcreteResultListFragment : Fragment() {
 
-    private var workBookNo:Int = 0
+    private var quizResult:QuizResult? = null
+
+    private val adapter:ConcreteResultListAdapter by lazy {
+        ConcreteResultListAdapter(quizResult?.resultTitle?:"")
+    }
+
+    private val viewModel: ResultViewModel by lazy {
+        ResultViewModelFactory(activity?.application!!).create(ResultViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            workBookNo = it.getInt(ResultFragment.SAFE_ARGS_KEY)
+            quizResult = it.get(ResultFragment.SAFE_ARGS_KEY) as QuizResult
         }
     }
 
     override fun onCreateView( inflater: LayoutInflater,
                                container: ViewGroup?,
                                savedInstanceState: Bundle? ): View? {
+
+        viewModel.quizWithHistory.observe(viewLifecycleOwner){
+            data->
+            adapter.submitList(
+                    data.filter { f-> f.quizEntity.relationWorkBook == quizResult?.relationWorkBookNo }
+                            .filter { f-> f.historyEntity.relationAccuracy == quizResult?.relationAccuracyNo}
+                            .sortedBy { it.historyEntity.historyQuizNumber }
+                            .toList()
+            )
+        }
         return inflater.inflate(R.layout.fragment_concrete_result_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        recycleInit()
     }
 
     private fun recycleInit(){
         recyclerview_concrete_result.also {
             recyclerView ->
-            recyclerView.adapter
+            recyclerView.adapter = adapter
             recyclerView.layoutManager =
                     LinearLayoutManager(activity).apply { orientation == LinearLayoutManager.VERTICAL }
-
         }
     }
 
