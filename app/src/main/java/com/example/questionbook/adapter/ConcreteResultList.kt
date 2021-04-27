8d6f866439f4a4b6e85955af138d99d66f72abe6
yusk178 <youjiebupo@gmail.com>
2021-04-27 10:30:51 +0900

    リザルト詳細一覧画面を表示する際の処理途中。

--- app/src/main/java/com/example/questionbook/adapter/ConcreteResultList.kt
+++ app/src/main/java/com/example/questionbook/adapter/ConcreteResultList.kt
@@ -1,4 +1,15 @@
 package com.example.questionbook.adapter
 
-class ConcreteResultList {
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ConcreteResultList:ListAdapter<>() {
+
+    companion object Diff:DiffUtil.ItemCallback<>
+
+    inner  class ConcreteResultListHolder(view:View):RecyclerView.ViewHolder(view){
+
+    }
 }
