af74d85e0eeb91355330d511bb4948c1d41083a7
yusk178 <youjiebupo@gmail.com>
2021-04-30 09:58:12 +0900

    MPAndroidChartライブラリーを使用して、棒グラフをひとまず表示。問題点としては、ラベルを現在時刻で表示させること、最大のy軸の設定が未達成なこと。

--- app/src/main/java/com/example/questionbook/fragment/StatisticsDetailsFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/StatisticsDetailsFragment.kt
@@ -1,24 +1,78 @@
 package com.example.questionbook.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.questionbook.R
+import com.example.questionbook.view_model.WorkBookViewModel
+import com.example.questionbook.view_model.WorkBookViewModelFactory
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import kotlinx.android.synthetic.main.fragment_statistics_details.*
 
 
+class StatisticsDetailsFragment : Fragment() {
 
-class AccuracyFragment : Fragment() {
+    private var workBookNo = 0
+
+    private val viewModel:WorkBookViewModel by lazy {
+        WorkBookViewModelFactory(activity?.application!!)
+                .create(WorkBookViewModel::class.java)
+    }
+
+    private val entryList = mutableListOf<BarEntry>()
+    private val barDataSets = mutableListOf<IBarDataSet>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            workBookNo = it.getInt(StatisticsFragment.SAFE_ARGS_KEY)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_accuracy,container,false)
+        return inflater.inflate(R.layout.fragment_statistics_details,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.data.observe(viewLifecycleOwner){
+            all->
+                var index = 0f
+                all.first { it.workBookEntity.workBookNo == workBookNo }
+                        .accuracyList.forEach {
+                            a-> entryList.add(BarEntry(index, a.accuracyRate))
+                            index += 1f
+                        }
+                set()
+            }
+    }
+    private fun set(){
+      val barDataSet = BarDataSet(entryList, "linear").also {
+            it.color = Color.BLUE
+      }
+        barDataSets.add(barDataSet)
+
+        val barData = BarData(barDataSets)
+
+        statistics_charts_barChartExample.apply {
+            data = barData
+            animateY(1000, Easing.EaseInOutQuad)
+            xAxis.also {
+                x->
+                x.isEnabled = true
+                x.textColor = Color.BLACK
+            }
+        }.invalidate()
     }
 }
