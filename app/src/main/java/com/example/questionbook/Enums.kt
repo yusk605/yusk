7335a2aa928f81255dfe7c9cc099307f571d6a7f
yusk178 <youjiebupo@gmail.com>
2021-05-06 20:28:44 +0900

    レイアウトファイル内に記述されている、onClick属性に指定されているメソッドないで遷移を行う際に使用する値を列挙クラスで管理

--- app/src/main/java/com/example/questionbook/Enums.kt
+++ app/src/main/java/com/example/questionbook/Enums.kt
@@ -1,13 +1,9 @@
 package com.example.questionbook
 
-/**
- * ■ ポップアップメニューを表示した時に選択した項目に値を持たせるためのクラス
- * @param item 選択した項目をInt型の数値で表す。
- */
-enum class PopupMenuSelectItems(val item:Int){
-    SELECT_FIRST(1),
-    SELECT_SECOND(2),
-    SELECT_THIRD(3),
-    NOT_SHOW(-1)
+
+enum class GarbageCanActionButton(val get:Int){
+    CATEGORY(1),
+    WORKBOOK(2),
+    LEAF(3),
  }
 
