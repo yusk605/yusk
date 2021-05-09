9a99642b190c66e55b3b69c04407f98269acb70e
yusk178 <youjiebupo@gmail.com>
2021-05-09 14:13:06 +0900

    削除確定タイトル欄から遷移を行った際に、受け渡す値を取得するコードを記述しました。

--- app/src/main/java/com/example/questionbook/fragment/GarbageCanListFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/GarbageCanListFragment.kt
@@ -1,35 +1,56 @@
-package com.example.questionbook
+package com.example.questionbook.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.questionbook.GarbageCanActionButton
+import com.example.questionbook.R
 
 class GarbageCanListFragment : Fragment() {
 
-    private var actionGarbageCan = 0
+    private var typeAction = 0
+
+    private var adapter: RecyclerView.Adapter<*>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-          actionGarbageCan = it.getInt(
-                resources.getStringArray(R.array.side_menu_keys)[4])
+            typeAction = it.getInt(
+                GarbageCanActionButton
+                    .CATEGORY
+                    .name
+                    .hashCode()
+                    .toString()
+            )
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_garbage_can_list, container, false)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
+    private fun selectAdapter(){
+        when(typeAction){
+            1 -> {
+            }
+            2 -> {
+            }
+            3 -> {
+            }
+            else ->{    }
+        }
+    }
 
     companion object {
         @JvmStatic
