4b0e87eba15d35baf419387d79e69731a3ef7c53
yusk178 <youjiebupo@gmail.com>
2021-05-09 18:48:09 +0900

    削除確定一覧画面でのフラグメント内処理を記述しました。

--- app/src/main/java/com/example/questionbook/fragment/GarbageCanListFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/GarbageCanListFragment.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckedTextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.questionbook.GarbageCanActionButton
 import com.example.questionbook.R
+import com.example.questionbook.room.QuestionCategoryEntity
+import com.example.questionbook.room.QuestionLeafEntity
+import com.example.questionbook.room.QuestionWorkBookEntity
+import com.example.questionbook.view_model.GarbageCanViewModel
+import com.example.questionbook.view_model.GarbageCanViewModelFactory
+import kotlinx.android.synthetic.main.fragment_garbage_can_list.*
 
 class GarbageCanListFragment : Fragment() {
 
     private var typeAction = 0
-
     private var adapter: RecyclerView.Adapter<*>? = null
+
+    private var arrayAdapter:ArrayAdapter<String>? = null
+
+    private val viewModel:GarbageCanViewModel by lazy {
+        GarbageCanViewModelFactory(requireActivity().application)
+            .create(GarbageCanViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,18 +52,57 @@ class GarbageCanListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initArrayAdapter()
+        val listView = garbage_can_choice_list
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener {
+                parent, view, position, id ->
+
+            var checkedTextView:CheckedTextView =
+                listView.getChildAt(position) as CheckedTextView
+
+            if (checkedTextView.isChecked){
+
+            }
+        }
+
     }
 
-    private fun selectAdapter(){
+    private fun getArrayAdapter(data:List<String>):ArrayAdapter<String>{
+        return ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_multiple_choice,
+            data
+        )
+    }
+
+    private fun initArrayAdapter(){
         when(typeAction){
             1 -> {
+                viewModel.categoryList.observe(viewLifecycleOwner){
+                    data ->
+                    arrayAdapter = getArrayAdapter(data.map { it.categoryTitle }.toList())
+                }
             }
             2 -> {
+                viewModel.workBookList.observe(viewLifecycleOwner){
+                    data ->
+                    arrayAdapter = getArrayAdapter(data.map { it.workBookTitle }.toList())
+                }
             }
             3 -> {
+                viewModel.leafList.observe(viewLifecycleOwner){
+                    data ->
+                    arrayAdapter = getArrayAdapter(data.map { it.leafStatement }.toList())
+                }
             }
-            else ->{    }
+            else -> throw IllegalAccessException(" selectAdapter not value ")
         }
+    }
+
+    private fun  initListView(){
+        garbage_can_choice_list.adapter = arrayAdapter
     }
 
     companion object {
