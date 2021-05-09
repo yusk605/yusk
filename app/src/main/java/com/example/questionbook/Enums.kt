228a0ac9cfd8b7ab8d1a091160815ecb6a80d830
yusk178 <youjiebupo@gmail.com>
2021-05-09 14:09:25 +0900

    サイドメニュー項目内にあるごみ箱をタップした際に値を送るために必要な列挙クラスの説明を行いました。

--- app/src/main/java/com/example/questionbook/Enums.kt
+++ app/src/main/java/com/example/questionbook/Enums.kt
@@ -1,6 +1,9 @@
 package com.example.questionbook
 
-
+/**
+ * 削除確定タイトル画面から遷移を行う際に渡す値。
+ * @param get 値の取得　CATEGORY..1 WORKBOOK..2 LEAF..3
+ */
 enum class GarbageCanActionButton(val get:Int){
     CATEGORY(1),
     WORKBOOK(2),
