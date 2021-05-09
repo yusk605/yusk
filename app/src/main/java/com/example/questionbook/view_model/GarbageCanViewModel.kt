f2f49aef843d9d2a8769c45659a4e186180a5f13
yusk178 <youjiebupo@gmail.com>
2021-05-09 14:16:56 +0900

    削除確定一覧を表示させるために必要なビューモデルを記述しました。

--- app/src/main/java/com/example/questionbook/view_model/GarbageCanViewModel.kt
+++ app/src/main/java/com/example/questionbook/view_model/GarbageCanViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.questionbook.view_model
 
-class GarbageCanViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questionbook.room.QuestionCategoryDao
+import com.example.questionbook.room.QuestionDatabase
+import com.example.questionbook.room.QuestionLeafDao
+import com.example.questionbook.room.QuestionWorkBookDao
+
+class GarbageCanViewModel(private val app:Application):AndroidViewModel(app) {
+
+    private val db = QuestionDatabase.getInstance(app,viewModelScope)
+
+    private val categoryDao:QuestionCategoryDao
+    private val workBookDao:QuestionWorkBookDao
+    private val leafDao:QuestionLeafDao
+
+    init {
+        db.run {
+            categoryDao = getCategoryDao()
+            workBookDao = getWorkBookDao()
+            leafDao     = getLeafDao()
+        }
+    }
+
+    private var _categoryList = categoryDao.getList()
+    private var _workBookList = workBookDao.getList()
+    private var _leafList     = leafDao.getList()
+
+    val categoryList get() = _categoryList
+    val workBookList get() = _workBookList
+    val leafList     get() = _leafList
 }
