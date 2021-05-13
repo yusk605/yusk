f7166fcf9b9060432c04e2ad8c5df0b199ac65ab
yusk178 <youjiebupo@gmail.com>
2021-05-13 21:59:47 +0900

    遷移先に値を送る際の属性名を変更、理由としては値を受け取る際に属性名の統一を行うことで、ボタンをクリックしたイベントごとに属性名を変える必要がないため。

--- app/src/main/java/com/example/questionbook/fragment/GarbageCanFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/GarbageCanFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.questionbook.GarbageCanActionButton
 import com.example.questionbook.MainActivity
 import com.example.questionbook.R
 import com.example.questionbook.newBundleToPutInt
+import kotlinx.android.synthetic.main.fragment_garbage_can.*
 
 class GarbageCanFragment : Fragment() {
 
@@ -34,9 +36,36 @@ class GarbageCanFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.safety()
+        val bundle  =   Bundle()
 
-        //TODO ボタンをクリックした時の処理を記述すること。　イベントハンドラーの場合はエラーが出るため。
+        val controller:NavController = Navigation.findNavController(view)
 
+        //分類一覧の項目を表示させる。
+        garbage_can_category_button.setOnClickListener {
+            bundle.putInt(
+                    ARGS_KEY,
+                    GarbageCanActionButton.CATEGORY.get
+            )
+            controller.navigate(R.id.action_garbageCanFragment_to_garbageCanListFragment,bundle)
+        }
+
+        //問題集一覧の項目を表示させる。
+        garbage_can_workbook_button.setOnClickListener {
+            bundle.putInt(
+                    ARGS_KEY,
+                    GarbageCanActionButton.WORKBOOK.get
+            )
+            controller.navigate(R.id.action_garbageCanFragment_to_garbageCanListFragment,bundle)
+        }
+
+        //問題文一覧の項目を表示させる。
+        garbage_can_leaf_button.setOnClickListener {
+            bundle.putInt(
+                    ARGS_KEY,
+                    GarbageCanActionButton.LEAF.get
+            )
+            controller.navigate(R.id.action_garbageCanFragment_to_garbageCanListFragment,bundle)
+        }
     }
 
     /**
@@ -60,25 +89,24 @@ class GarbageCanFragment : Fragment() {
     fun actionButton(view: View){
         val bundle  =   Bundle()
         when(view.id){
-            R.id.garbage_can_category_button -> bundle.putInt(
-                GarbageCanActionButton.CATEGORY.name.hashCode().toString(),
-                GarbageCanActionButton.CATEGORY.get
-            )
-            R.id.garbage_can_workbook_button -> bundle.putInt(
-                GarbageCanActionButton.WORKBOOK.name.hashCode().toString(),
-                GarbageCanActionButton.WORKBOOK.get
-            )
-            R.id.garbage_can_leaf_button    -> bundle.putInt(
-                GarbageCanActionButton.LEAF.name.hashCode().toString(),
-                GarbageCanActionButton.LEAF.get
-            )
+            R.id.garbage_can_category_button ->
+                bundle.putInt(
+                    ARGS_KEY, GarbageCanActionButton.CATEGORY.get
+                )
+            R.id.garbage_can_workbook_button ->
+                bundle.putInt(
+                    ARGS_KEY, GarbageCanActionButton.WORKBOOK.get
+                )
+            R.id.garbage_can_leaf_button ->
+                bundle.putInt(
+                    ARGS_KEY, GarbageCanActionButton.LEAF.get
+                )
         }
         Navigation.findNavController(view)
-            .navigate(
-                R.id.action_garbageCanFragment_to_garbageCanListFragment,
-                bundle
-            )
+            .navigate(R.id.action_garbageCanFragment_to_garbageCanListFragment, bundle)
         }
 
-    companion object { }
+    companion object {
+        const val ARGS_KEY = "garbageCanFragment_to_garbageCanListFragment"
+    }
 }
