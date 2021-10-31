c028d1d4a7a1a26a3e24c2cca40f3b46b523c2f0
不破佑介 <80320642+yusk605@users.noreply.github.com>
2021-10-31 09:52:39 +0900

    自動修正が入りました。またライブラリーの追加

--- app/src/main/java/com/example/questionbook/fragment/GameStartFragment.kt
+++ app/src/main/java/com/example/questionbook/fragment/GameStartFragment.kt
@@ -1,12 +1,11 @@
 package com.example.questionbook.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
-import androidx.lifecycle.LifecycleOwner
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.questionbook.*
 import com.example.questionbook.databinding.FragmentGameStartBinding
